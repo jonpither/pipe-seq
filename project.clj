e603d04c5172acb32abd38a66e6ed5415e92e006
Jon Pither <jon.pither@gmail.com>
2013-04-17 09:05:31 +0100

    added code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pipe-seq "0.1.0"
+(defproject pipe-seq "0.1.0-SNAPSHOT"
   :description "pmap styled pipes"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/pipe_seq/core.clj
+++ src/pipe_seq/core.clj
@@ -1,6 +1,54 @@
 (ns pipe-seq.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn pipe
+  "Returns a vector containing a sequence that will read from the
+   queue, and a function that inserts items into the queue.
+
+   Copyright Christophe Grand
+   See http://clj-me.cgrand.net/2010/04/02/pipe-dreams-are-not-necessarily-made-of-promises/"
+  [size]
+  (let [q (if size
+            (java.util.concurrent.LinkedBlockingQueue. size)
+            (java.util.concurrent.LinkedBlockingQueue.))
+        EOQ (Object.)
+        NIL (Object.)
+        s (fn s [] (lazy-seq (let [x (.take q)]
+                               (when-not (= EOQ x)
+                                 (cons (when-not (= NIL x) x) (s))))))]
+    [(s) (fn ([] (.put q EOQ)) ([x] (.put q (or x NIL))))]))
+
+(defn pipe-seq
+  "See http://www.pitheringabout.com/?p=874
+
+   Consumes the col with function f returning a new lazy seq.
+   The consumption is done in parallel using n-threads backed
+   by a queue of the specified size. The output sequence is also
+   backed by a queue of the same given size."
+  [f n-threads pipe-size col]
+  (let [q (java.util.concurrent.LinkedBlockingQueue. pipe-size)
+        finished-feeding (promise)
+        latch (java.util.concurrent.CountDownLatch. n-threads)
+        [out-seq out-queue] (pipe pipe-size)]
+
+    ;; Feeder thread
+    (future
+      (doseq [v (remove nil? col)]
+        (.put q v))
+      (deliver finished-feeding true))
+
+    (dotimes [i n-threads]
+      (future (try (loop []
+                     (let [v (.poll q 50 java.util.concurrent.TimeUnit/MILLISECONDS)]
+                       (when v (out-queue (f v)))
+                       (when-not (and (zero? (.size q))
+                                      (realized? finished-feeding))
+                         (recur))))
+                   (finally
+                     (.countDown latch)))))
+
+    ;; Supervisor thread
+    (future
+      (.await latch)
+      (out-queue))
+
+    out-seq))
