cbf9468b6bee502c71115d2a538b94acbc4c0d15
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2013-07-10 16:02:13 +0100

    adds non blocking pipe feeding option

--- README.md
+++ README.md
@@ -11,9 +11,13 @@ See http://www.pitheringabout.com/?p=874
       (pipe-seq some-fn 10 1)
       doall)`
 
+feed the pipe in a nonblocking manner with a limited queue size:
+
+`(let [[s f] (pipe :size 5 :feed-non-blocking 1)] (dotimes [n 10] (f n)) (doseq [x s] (println x)))
+
 ## Install
 
-:dependencies [[pipe-seq "0.1.1"]]
+:dependencies [[pipe-seq "0.2.1"]]
 
 ## License
 
--- src/pipe_seq/core.clj
+++ src/pipe_seq/core.clj
@@ -6,18 +6,27 @@
   "Returns a vector containing a sequence that will read from the
    queue, and a function that inserts items into the queue.
 
+   If whatever value provided by the key :feed-non-blocking queue will be fed in a
+   non blocking manner using offer insteat of put.
    Copyright Christophe Grand
    See http://clj-me.cgrand.net/2010/04/02/pipe-dreams-are-not-necessarily-made-of-promises/"
-  [size]
-  (let [q (if size
+  [& {:keys [size feed-non-blocking] :as args}]
+  (let [size (:size args)
+        fnb (contains?  args :feed-non-blocking)
+        q (if size
             (java.util.concurrent.LinkedBlockingQueue. size)
             (java.util.concurrent.LinkedBlockingQueue.))
         EOQ (Object.)
         NIL (Object.)
+        feed-fn (if fnb
+                  (fn [x] (if-not (.offer q (or x NIL))
+                            (log/warn x " was rejected")))
+                  (fn [x] (.put q (or x NIL))))
         s (fn s [] (lazy-seq (let [x (.take q)]
                                (when-not (= EOQ x)
                                  (cons (when-not (= NIL x) x) (s))))))]
-    [(s) (fn ([] (.put q EOQ)) ([x] (.put q (or x NIL))))]))
+    [(s) (fn ([] (.put q EOQ))
+           ([x] (feed-fn x)))]))
 
 (defn pipe-seq
   "See http://www.pitheringabout.com/?p=874
@@ -30,7 +39,7 @@
   (let [q (java.util.concurrent.LinkedBlockingQueue. pipe-size)
         finished-feeding (promise)
         latch (java.util.concurrent.CountDownLatch. n-threads)
-        [out-seq out-queue] (pipe pipe-size)]
+        [out-seq out-queue] (pipe :size pipe-size)]
 
     ;; Feeder thread
     (future
