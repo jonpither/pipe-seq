80c274ab8b2b9b75f41a37dc0d7f971794a46026
Jon Pither <jon.pither@gmail.com>
2013-07-17 08:52:51 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pipe-seq "0.3.0"
   :description "pmap styled pipes"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jonpither/pipe-seq"
   :license {:name "Eclipse Public License"
             :url "https://github.com/jonpither/pipe-seq"}
   :dependencies [[org.clojure/clojure "1.5.1"]
